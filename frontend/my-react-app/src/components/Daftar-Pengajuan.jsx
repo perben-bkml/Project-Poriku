12df86d5e89d38e81bdc37bc0cf7a90610fe2526
Hexsia <josearmandohimang@gmail.com>
2025-01-15 15:33:04 +0700

    6th commit. Add new skeleton for Bendahara-menu content. Add Daftar-Pengajuan component. Add logo bakamla, Material UI avatar, and section for profile picture.

---
+++ frontend/my-react-app/src/components/Daftar-Pengajuan.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function DaftarPengajuan(){
+    return (
+        <div className="pengajuan bg-card">
+            <div className="pengajuan-filter">
+                
+            </div>
+            <div className="pengajuan-content">
+
+            </div>
+        </div>
+    )
+}
+
+export default DaftarPengajuan;
--- frontend/my-react-app/src/pages/Bendahara-main.jsx
+++ frontend/my-react-app/src/pages/Bendahara-main.jsx
@@ -1,21 +1,32 @@
 import React from "react";
+// Import Components
+import DaftarPengajuan from "../components/Daftar-Pengajuan";
 // Material UI icons
 import AssignmentIcon from '@mui/icons-material/Assignment';
 import ChecklistIcon from '@mui/icons-material/Checklist';
-import { createTheme } from "@mui/material";
+import Avatar from "@mui/material/Avatar";
 
 function Bendahara() {
+    const contentTitle = ["Daftar Pengajuan", "Lihat Antrian"]
+
+
     return (
         <div className="bendahara-home">
             <div className="dash-title">
                 <h2>Menu<br /> Bendahara</h2>
             </div>
             <div className="page-content">
-                <h1>Test Page</h1>
+                <h1 className="content-title">{contentTitle[0]}</h1>
+                <DaftarPengajuan />
             </div>
             <div className="dash-content">
                 <button><AssignmentIcon fontSize="small"/><span className="padd-span-bend"/>Daftar Pengajuan</button>
                 <button><ChecklistIcon fontSize="small"/><span className="padd-span-bend"/>Lihat Antrian</button>
+            </div>
+            <div className="dash-user">
+                <Avatar sx={{width: 35, height: 35}} alt="bakamla-logo" src="../../public/assets/bakamla_logo.png" />
+                <span className="padd-span-bend"></span>
+                <p>Biro Umum</p>
             </div>
         </div>
     )

57e8abb6f8aa1b430ca93d5edf4a3b00078da315
perbend-bkml <perbend.bakamla@gmail.com>
2025-05-18 22:18:12 +0700

    restructure isloading and isauth on Landing.jsx

--- frontend/my-react-app/src/pages/Landing.jsx
+++ frontend/my-react-app/src/pages/Landing.jsx
@@ -16,7 +16,7 @@ export default function Landing() {
                 <h1>Bakamla RI</h1>
             </div>
             <div className="landing-buttons slide-down">
-                <NavLink to={isAuthenticated ? "/home" : "/login"}><button className='page-button'>Layanan Ajuan</button></NavLink>
+                <NavLink to={isLoading && isAuthenticated ? "/home" : "/login"}><button className='page-button'>Layanan Ajuan</button></NavLink>
                 <NavLink to="/layanan-gaji"><button className='page-button'>Layanan Gaji</button></NavLink>
             </div>
         </div>
